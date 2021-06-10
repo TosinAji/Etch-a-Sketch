d3e3ab8f9aedb679f6a4be056c5b2c6b85798390
Tosin Ajimobi <tosinajimobi@gmail.com>
2021-06-10 17:06:11 +0100

    divs are coloured on hover

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ for (let current = 0; current < 256; current++ ) {
     gridSection.classList.add('grid-section');
     grid.appendChild(gridSection);
 }
+
+const gridSections = document.querySelectorAll('.grid-section');
+gridSections.forEach(section => section.addEventListener('mouseenter', changeColour));
+
+function changeColour(event) {
+    this.classList.add('hover');
+};
