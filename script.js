f0b4b25ee02c0fedea3b77dc4e824ba9cb03ef12
Tosin Ajimobi <tosinajimobi@gmail.com>
2021-06-10 23:47:51 +0100

    added a button to reset the board and choose the number of sections

--- script.js
+++ script.js
@@ -2,15 +2,35 @@
 
 const grid = document.querySelector('#grid');
 
-for (let current = 0; current < 256; current++ ) {
-    const gridSection = document.createElement('div');
-    gridSection.classList.add('grid-section');
-    grid.appendChild(gridSection);
-}
+// const gridSections = document.querySelectorAll('.grid-section');
+// gridSections.forEach(section => section.addEventListener('mouseenter', changeColour));
 
-const gridSections = document.querySelectorAll('.grid-section');
-gridSections.forEach(section => section.addEventListener('mouseenter', changeColour));
+const resetButtton = document.querySelector('#reset');
+resetButtton.addEventListener('click', resetBoard);
+
+
+createNewBoard(16);
 
 function changeColour(event) {
     this.classList.add('hover');
+}; 
+
+function resetBoard(event) {
+    let size = +prompt("Next board size ", "16");
+
+    const gridSections = document.querySelectorAll('.grid-section');
+    gridSections.forEach(section => section.remove());
+    createNewBoard(size);
+};
+
+function createNewBoard(size) {
+    for (let current = 0; current < (size*size); current++ ) {
+        const gridSection = document.createElement('div');
+        gridSection.classList.add('grid-section');
+        grid.appendChild(gridSection);
+    }
+
+    const gridSections = document.querySelectorAll('.grid-section');
+    gridSections.forEach(section => section.addEventListener('mouseenter', changeColour));
+
 };
